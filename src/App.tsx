f540fcc098ce7533b0c109f5c13130d6061fa30b
Viktor Elofsson <viktor@viktorelofsson.se>
2026-06-02 15:33:45 +0200

    new vite project

--- src/App.tsx
+++ src/App.tsx
@@ -1,171 +1,122 @@
-import { Flex, Grid, GridItem, HStack, IconButton, Image, Link, Spacer, Stack, Text, useColorMode } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { MdDarkMode, MdExtension, MdLightMode, MdOutlineCameraAlt, MdSettings } from 'react-icons/md';
-import { SiDiscord } from 'react-icons/si';
-import { Navigate, NavLink, Outlet } from 'react-router-dom';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
 
-import Isotype from './assets/isotype.svg';
-import prefixPath from './base';
-import AppErrorModal from './components/AppErrorModal';
-import Query from './components/Query';
-import SettingsDrawer from './components/SettingsDrawer';
-import useAuth from './contexts/auth';
-import useNinja from './contexts/ninja';
-import { AuthError, useRPC } from './services/jsonrpc';
-
-type IVersionInfo = {
-  porla: {
-    commitish: string;
-    branch: string;
-    version: string;
-  }
-}
-
-function AuthApp() {
-  const { data, error } = useRPC<IVersionInfo>("sys.versions");
-  const [ showSettings, setShowSettings ] = useState(false);
-  const { colorMode, toggleColorMode } = useColorMode();
-  const { isNinja, toggleNinja } = useNinja();
-
-  if (error && error instanceof AuthError) {
-    return <Navigate to="/login" />
-  }
-
-  if (error) {
-    return (
-      <AppErrorModal error={error} />
-    );
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <SettingsDrawer
-        isOpen={showSettings}
-        onClose={() => setShowSettings(false)}
-      />
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
 
-      <Grid
-        height={"100vh"}
-        templateAreas={`
-          "header"
-          "main"
-          "footer"
-        `}
-        gridTemplateColumns={"1fr"}
-        gridTemplateRows={"0fr 1fr 0fr"}
-      >
-        <GridItem area={"header"}>
-          <Flex
-            borderBottomColor={"whiteAlpha.400"}
-            borderBottomWidth={"1px"}
-            boxShadow={"sm"}
-            justifyContent={"space-between"}
-            mb={2}
-            p={3}
-          >
-            <Stack direction={"row"} alignItems={"center"} spacing={5}>
-              <NavLink to="/">
-                <Image src={Isotype} width={"32px"} />
-              </NavLink>
-            </Stack>
+      <div className="ticks"></div>
 
-            <Query />
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
 
-            <Flex>
-              <IconButton
-                aria-label="Plugins"
-                icon={<MdExtension />}
-                variant={"link"}
-                as={NavLink}
-                to="/plugins"
-              />
-
-              <IconButton
-                aria-label="Settings"
-                icon={<MdSettings />}
-                variant={"link"}
-                as={NavLink}
-                to="/settings"
-              />
-            </Flex>
-          </Flex>
-        </GridItem>
-
-        <GridItem area={"main"} overflow={"auto"}>
-          <Outlet />
-        </GridItem>
-
-        <GridItem area={"footer"}>
-          <HStack
-            justifyContent={"end"}
-            padding={1}
-            backgroundColor={colorMode === "dark" ? "whiteAlpha.50" : ""}
-            borderTopColor={colorMode === "dark" ? "whiteAlpha.200" : ""}
-            borderTopWidth={"1px"}
-            color={colorMode === "dark" ? "whiteAlpha.600" : ""}
-          >
-            <IconButton
-              aria-label={""}
-              icon={colorMode === "dark" ? <MdLightMode /> : <MdDarkMode />}
-              onClick={() => toggleColorMode()}
-              size={"xs"}
-              variant={"link"}
-            />
-
-            <IconButton
-              aria-label={"Join Porla on Discord"}
-              as={Link}
-              href="https://discord.gg/FNVVRzqJAG"
-              icon={<SiDiscord />}
-              isExternal
-              size={"xs"}
-              title={"Join Porla on Discord"}
-              variant={"link"}
-            />
-
-            <IconButton
-              aria-label={"Toggle Ninja mode"}
-              color={isNinja ? "red.400" : ""}
-              icon={<MdOutlineCameraAlt />}
-              onClick={() => toggleNinja()}
-              size={"xs"}
-              variant={"link"}
-            />
-
-            <Spacer />
-            <Text fontSize={"xs"}>{data?.porla.version === "" ? "dev" : data?.porla.version}</Text>
-            { data?.porla.branch    !== "-" && <Text fontSize={"xs"}>{data?.porla.branch}</Text> }
-            { data?.porla.commitish !== "-" && <Text fontSize={"xs"}>{data?.porla.commitish.substring(0,7)}</Text> }
-          </HStack>
-        </GridItem>
-      </Grid>
+      <div className="ticks"></div>
+      <section id="spacer"></section>
     </>
   )
 }
 
-export default function App() {
-  const { user } = useAuth();
-  const [status, setStatus] = useState<string | undefined>();
-
-  useEffect(() => {
-    fetch(prefixPath("/api/v1/system"))
-      .then(r => r.json())
-      .then(s => {
-        setStatus(s.status);
-      });
-  }, []);
-
-  if (!status) {
-    return <div>Loading</div>
-  }
-
-  if (status === "setup") {
-    return <Navigate to={"/setup"} />
-  }
-
-  if (!user) {
-    return <Navigate to={"/login"} />
-  }
-
-  return <AuthApp />
-}
+export default App
