a53712cbddf750d0a4887e3340a8dced90f063ad
Viktor Elofsson <viktor@viktorelofsson.se>
2026-06-02 17:02:08 +0200

    login

--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/_layout")({
 
 function RouteComponent() {
   const query = useQuery({
-    queryKey: [""],
+    queryKey: ["sys.status"],
     queryFn: () =>
       fetch("/api/v1/jsonrpc", {
         method: "POST",
@@ -24,6 +24,32 @@ function RouteComponent() {
 
   if (query.data && query.data.result.status === "setup") {
     return <Navigate to="/setup" />;
+  }
+
+  return <SysVersionsLayout />;
+}
+
+function SysVersionsLayout() {
+  const versions = useQuery({
+    queryKey: ["sys.versions"],
+    queryFn: () =>
+      fetch("/api/v1/jsonrpc", {
+        method: "POST",
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          method: "sys.versions",
+        }),
+        credentials: "include",
+      }).then((r) => r.json()),
+    networkMode: "always",
+  });
+
+  if (versions.isLoading) {
+    return <>loading versions</>;
+  }
+
+  if (versions.data.error && versions.data.error.code === 1001) {
+    return <Navigate to="/login" />;
   }
 
   return <Outlet />;
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,9 +1,71 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useForm } from "@tanstack/react-form";
+import { useMutation } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
+import clsx from "clsx";
 
-export const Route = createFileRoute('/login')({
+export const Route = createFileRoute("/login")({
   component: RouteComponent,
-})
+});
+
+type AuthLoginReq = {
+  username: string;
+  password: string;
+};
 
 function RouteComponent() {
-  return <div>Hello "/login"!</div>
+  const navigate = Route.useNavigate();
+
+  const authInit = useMutation({
+    mutationKey: ["auth.login"],
+    mutationFn: (init: AuthLoginReq) =>
+      fetch("/api/v1/jsonrpc", {
+        method: "POST",
+        body: JSON.stringify({
+          jsonrpc: "2.0",
+          method: "auth.login",
+          params: init,
+        }),
+      }).then((r) => r.json()),
+  });
+
+  const form = useForm({
+    defaultValues: {
+      username: "foo",
+      password: "bar",
+    },
+    onSubmit: async ({ value }) => {
+      const r = await authInit.mutateAsync(value);
+      await navigate({ to: "/" });
+    },
+  });
+
+  return (
+    <div className="m-4">
+      <h1>Log in to Porla</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <fieldset className="fieldset">
+          <label className="label">Username</label>
+          <input type="text" className="input" placeholder="porla-user-ab12" />
+
+          <label className="label">Password</label>
+          <input type="password" className="input" />
+        </fieldset>
+        <button
+          type="submit"
+          className={clsx([
+            "btn btn-primary",
+            form.state.isSubmitting && "btn-disabled",
+          ])}
+        >
+          Log in
+        </button>
+      </form>
+    </div>
+  );
 }
