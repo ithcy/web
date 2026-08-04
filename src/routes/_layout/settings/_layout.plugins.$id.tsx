97407f557e5ca6f6def8d3ee7415211e0ae042ab
Viktor Elofsson <viktor@viktorelofsson.se>
2026-08-04 16:07:19 +0200

    wip

--- src/routes/_layout/settings/_layout.plugins.$id.tsx
+++ src/routes/_layout/settings/_layout.plugins.$id.tsx
@@ -1,9 +1,44 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useInvoker } from "@/api";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_layout/settings/_layout/plugins/$id')({
+export const Route = createFileRoute("/_layout/settings/_layout/plugins/$id")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_layout/settings/_layout/plugins/$id"!</div>
+  const navigate = Route.useNavigate();
+  const { id } = Route.useParams();
+
+  const reload = useInvoker("plugins.reload");
+  const remove = useInvoker("plugins.remove");
+
+  return (
+    <div className="flex space-x-5">
+      <button
+        disabled={reload.isPending}
+        className="btn btn-primary"
+        onClick={async () => {
+          await reload.mutateAsync({
+            id: parseInt(id),
+          });
+        }}
+      >
+        Reload
+      </button>
+
+      <button
+        disabled={remove.isPending}
+        className="btn btn-warning"
+        onClick={async () => {
+          await remove.mutateAsync({
+            id: parseInt(id),
+          });
+
+          await navigate({ to: "/settings" });
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
 }
